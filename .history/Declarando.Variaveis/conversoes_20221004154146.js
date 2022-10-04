da361e22a8425ba0c606a0bfb4048e8ceda0af80
Pamela Rabech <pamela_cwb@live.com>
2022-10-04 16:00:13 -0300

    declarando variaveis

---
+++ .history/Declarando.Variaveis/conversoes_20221004154146.js
@@ -0,0 +1,9 @@
+// tipo de dado
+// booleanos 
+
+// conversão implicita 
+
+const numero = 456;
+const numerostring = "456";
+
+console.log(numero == numerostring)
---
+++ .history/Declarando.Variaveis/conversoes_20221004155315.js
@@ -0,0 +1,14 @@
+// tipo de dado
+// booleanos 
+
+// conversão implicita 
+
+const numero = 456;
+const numerostring = "456";
+
+//number ()
+//string ()
+console.log(numero + Number(numerostring))
+
+// conversão explicita
+
